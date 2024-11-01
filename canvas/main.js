61841ddb76493f4d5b4c6e58a14203b4106be9e4
Jcambareri6 <joaquin.cambareri@eest3necochea.edu.ar>
2024-11-01 09:28:39 -0300

    .

--- canvas/main.js
+++ canvas/main.js
@@ -1,17 +1,17 @@
     let canvas = document.getElementById("canvasP");
     let ctx = canvas.getContext('2d');
  ctx.fillStyle = "#FF0000";  
-    let img = new Image();
-    img.src="./img/fondoCanvasPresentacion.png"
-    img.onload = function() {
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    };
-    ctx.beginPath();
+    // let img = new Image();
+    // img.src="./img/fondoCanvasPresentacion.png"
+    // img.onload = function() {
+    //     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    // };
+    // ctx.beginPath();
     
    
     
-        // let juego1= new Juego(6,canvas,ctx);
-        // juego1.play();
+        let juego1= new Juego(6,canvas,ctx);
+        juego1.play();
         
        
       
